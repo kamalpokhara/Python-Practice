ae19cc01faa0ba3c1c585a6be3d524d5d02feb6f
kamal <poudelkamal000@gmail.com>
2026-02-25 11:53:21 +0545

    feat(decorator): added decorator

---
+++ python-foundations/decorator.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+
+# username = input("Enter your user name")
+def logger(func):
+    """
+    Wrapper
+
+    it dynamically shows username
+
+    """
+
+    def wrapper(*args, **kwargs):
+        print(f"{args[0]} has called {func.__name__} at {datetime.now()} ")
+        func(*args, **kwargs)
+        print(f"{args[0]} has ended hello at {datetime.now()} ")
+
+    return wrapper
+
+
+@logger
+def hello(username):
+    print("hello ", username)
+
+
+@logger
+def goodbye(username):
+    print("bye bye ", username)
+
+
+@logger
+def login(username):
+    print("User is tryna login ", username)
+
+
+@logger
+def logout(username):
+    print("User is tryna logout ", username)
+
+
+hello("kamal")
---
+++ python-foundations/exception.py
@@ -0,0 +1,33 @@
+# a = int(input("Enter the divident "))
+# b = int(input("Enter the divider "))
+# numbers = [1, 2, 3, 4, 5, 6, 7]
+# i = int(input("Enter the numbrt ehich u want to access "))
+# try:
+#     print("Result is ", a / b)
+#     print(numbers[i])
+
+# except ZeroDivisionError:
+#     print(f"Cannot divide {a} by zero")
+
+# except IndexError:
+#     print(f"Data doesnot exist at {i}")
+
+# except:
+#     print(f"unknown error occured: error: {e}")
+
+
+# print("This access has been logged")
+
+# empty = []
+# cont = 0
+# while True:
+#     try:
+#         print("hellow")
+#         cont+= 1
+#         print(cont)
+#         empty.append(cont)
+#     except KeyboardInterrupt:
+#         print("You entered ctrl + break, but I wont break")
+
+
+emptydic = dict() #empty dic
